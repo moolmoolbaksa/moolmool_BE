ad310b00571e48616adbb484e720fc93758982d0
langho968 <langho968@gmail.com>
2022-04-25 10:37:25 +0900

    기본 셋팅

---
+++ src/main/java/com/sparta/mulmul/MulmulApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.mulmul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MulmulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MulmulApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/mulmul/MulmulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.mulmul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MulmulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
