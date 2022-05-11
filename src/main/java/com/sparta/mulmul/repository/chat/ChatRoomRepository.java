09f046f4503e42e6aa17e487931d6d040f27afc7
hwangseokjun <unbeatablesj@naver.com>
2022-05-11 19:45:07 +0900

    Feat: 3차 머지

--- src/main/java/com/sparta/mulmul/repository/chat/ChatRoomRepository.java
+++ src/main/java/com/sparta/mulmul/repository/chat/ChatRoomRepository.java
@@ -3,6 +3,8 @@ package com.sparta.mulmul.repository.chat;
 import com.sparta.mulmul.model.ChatRoom;
 import com.sparta.mulmul.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
