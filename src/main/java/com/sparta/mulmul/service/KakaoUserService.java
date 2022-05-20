685dd3c39e2455e1772ae40e23c2a2778b63761a
langho968 <langho968@gmail.com>
2022-05-20 15:27:00 +0900

    Fix : 이미지 리사이즈 수정

--- src/main/java/com/sparta/mulmul/service/KakaoUserService.java
+++ src/main/java/com/sparta/mulmul/service/KakaoUserService.java
@@ -57,7 +57,7 @@ public class KakaoUserService {
         body.add("grant_type", "authorization_code");
         body.add("client_id", "6c57a62de555a589bbaaabdc73a9e011");
         //https://moolmooldoctor.firebaseapp.com/auth/kakao/callback
-        body.add("redirect_uri", "http://localhost:3000/auth/kakao/callback");
+        body.add("redirect_uri", "https://moolmooldoctor.firebaseapp.com/auth/kakao/callback");
         body.add("code", code);
         // HTTP 요청 보내기
         HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest =
