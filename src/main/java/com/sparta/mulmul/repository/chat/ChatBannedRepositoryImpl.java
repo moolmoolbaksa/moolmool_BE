d3646ca8c6ed5b883f1948eb24485002442daaf7
hwangseokjun <unbeatablesj@naver.com>
2022-05-23 12:57:07 +0900

    Feat: 차단 해제, 차단 목록보기 추가

--- src/main/java/com/sparta/mulmul/repository/chat/ChatBannedRepositoryImpl.java
+++ src/main/java/com/sparta/mulmul/repository/chat/ChatBannedRepositoryImpl.java
@@ -1,7 +1,11 @@
 package com.sparta.mulmul.repository.chat;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.sparta.mulmul.model.ChatBanned;
 import com.sparta.mulmul.model.User;
+
+import java.util.List;
+import java.util.Optional;
 
 import static com.sparta.mulmul.model.QChatBanned.*;
 
@@ -14,14 +18,46 @@ public class ChatBannedRepositoryImpl implements BannedQuerydsl {
     }
 
     @Override
+    public Boolean existsByUsers(User user, User bannedUser){
+        Integer fetchOne = queryFactory
+                .selectOne()
+                .from(chatBanned)
+                .where(chatBanned.user.id.eq(user.getId()).or(chatBanned.bannedUser.id.eq(bannedUser.getId())),
+                        chatBanned.user.id.eq(bannedUser.getId()).or(chatBanned.bannedUser.id.eq(user.getId())),
+                        chatBanned.isBanned.eq(true))
+                .fetchFirst();
+        return fetchOne != null;
+    }
+
+    @Override
+    public List<User> findAllMyBannedByUser(User user){
+        return queryFactory
+                .select(chatBanned.bannedUser)
+                .from(chatBanned)
+                .where(chatBanned.user.eq(user), chatBanned.isBanned.eq(true))
+                .fetch();
+    }
+
+    @Override // 헷갈리므로 리네임 필요합니다.
     public Boolean existsByUser(User user, User bannedUser){
         Integer fetchOne = queryFactory
                 .selectOne()
                 .from(chatBanned)
                 .where(chatBanned.user.id.eq(user.getId()).or(chatBanned.bannedUser.id.eq(bannedUser.getId())),
-                        chatBanned.user.id.eq(bannedUser.getId()).or(chatBanned.bannedUser.id.eq(user.getId()))
-                ).fetchFirst();
+                        chatBanned.isBanned.eq(true))
+                .fetchFirst();
         return fetchOne != null;
-    };
+    }
+
+    @Override
+    public Optional<ChatBanned> findByUsers(User user, User bannedUser){
+
+        return Optional.ofNullable(queryFactory
+                .selectFrom(chatBanned)
+                .where(
+                        chatBanned.user.eq(user),
+                        chatBanned.bannedUser.eq(bannedUser))
+                .fetchOne());
+    }
 
 }
