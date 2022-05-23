da15b8cec26b3ed4271a76e25f6c8c6ae81a9bd3
hwangseokjun <unbeatablesj@naver.com>
2022-05-23 16:19:54 +0900

    Feat: 알림 기능 보기 추가

--- src/main/java/com/sparta/mulmul/controller/NotificationController.java
+++ src/main/java/com/sparta/mulmul/controller/NotificationController.java
@@ -81,4 +81,20 @@ public class NotificationController {
         notificationRepository.deleteById(notificationId);
         return ResponseEntity.ok().body(OkDto.valueOf("true"));
     }
+
+    // 교환 신청 취소
+    @GetMapping("/notification/{notificationId}/cancel") // 교환 신청 취소에 대해 이 주소로 요청을 보내면 작동합니다.
+    public ResponseEntity<OkDto> cancelBarter(@PathVariable Long notificationId){
+
+        notificationService.setRead(notificationId);
+        return ResponseEntity.ok().body(OkDto.valueOf("true"));
+    }
+
+    // 회원 가입 메시지
+    @GetMapping("/notification/{notificationId}/signup") // 회원가입 축하 메시지에 대해 이 주소로 요청을 보내면 작동합니다.
+    public ResponseEntity<OkDto> signup(@PathVariable Long notificationId){
+
+        notificationService.setRead(notificationId);
+        return ResponseEntity.ok().body(OkDto.valueOf("true"));
+    }
 }
