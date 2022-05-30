2bf9f811012a22260ebe005205ce9c53c806ac7b
langho968 <langho968@gmail.com>
2022-05-30 15:41:46 +0900

    CICD test

---
+++ src/main/java/com/sparta/mulmul/utils/CICDController.java
@@ -0,0 +1,14 @@
+package com.sparta.mulmul.utils;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class CICDController {
+    @GetMapping("/health")
+    public String checkHealth() {
+        return "healthy";
+    }
+}
