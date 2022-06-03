b1fd9b74535b3ca2ad0e915d7bd115c26b846cd0
langho968 <langho968@gmail.com>
2022-06-03 11:59:12 +0900

    test : jasypt테스트 코드 주석처리

--- src/test/java/com/sparta/mulmul/service/JasyptConfigTest.java
+++ src/test/java/com/sparta/mulmul/service/JasyptConfigTest.java
@@ -1,38 +1,38 @@
-package com.sparta.mulmul.service;
-
-import org.assertj.core.api.Assertions;
-import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
-import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
-
-import org.junit.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-public class JasyptConfigTest {
-    @Test
-    public void jasypt_test() {
-        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
-        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-<<<<<<< HEAD
-=======
-//        config.setPassword(System.getenv("JASYPT_PASSWORD"));
->>>>>>> 5bd0b4fdc48fdc9e5b6ca4d6504108ee2b278793
-        config.setAlgorithm("PBEWithMD5AndDES");
-        config.setKeyObtentionIterations("1000");
-        config.setPoolSize("1");
-        config.setProviderName("SunJCE");
-        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
-        config.setIvGeneratorClassName("org.jasypt.iv.RandomIvGenerator");
-        config.setStringOutputType("base64");
-        encryptor.setConfig(config);
-
-        String encryptText = encryptor.encrypt(plaintText);
-        System.out.println(encryptText);
-        String decryptText = encryptor.decrypt(encryptText);
-        System.out.println(decryptText);
-        Assertions.assertThat(plaintText).isEqualTo(decryptText);
-
-    }
-
-}
+//package com.sparta.mulmul.service;
+//
+//import org.assertj.core.api.Assertions;
+//import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+//import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+//
+//import org.junit.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//public class JasyptConfigTest {
+//    @Test
+//    public void jasypt_test() {
+//        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+//        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+//<<<<<<< HEAD
+//=======
+////        config.setPassword(System.getenv("JASYPT_PASSWORD"));
+//
+//        config.setAlgorithm("PBEWithMD5AndDES");
+//        config.setKeyObtentionIterations("1000");
+//        config.setPoolSize("1");
+//        config.setProviderName("SunJCE");
+//        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+//        config.setIvGeneratorClassName("org.jasypt.iv.RandomIvGenerator");
+//        config.setStringOutputType("base64");
+//        encryptor.setConfig(config);
+//
+//        String encryptText = encryptor.encrypt(plaintText);
+//        System.out.println(encryptText);
+//        String decryptText = encryptor.decrypt(encryptText);
+//        System.out.println(decryptText);
+//        Assertions.assertThat(plaintText).isEqualTo(decryptText);
+//
+//    }
+//
+//}
 
